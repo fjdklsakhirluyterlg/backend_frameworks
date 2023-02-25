304320e3b8d8f54c1cef0f3bd5e945da1aa73b41
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-25 22:28:17 +0000

    stuff

--- python/sratch/paths.py
+++ python/sratch/paths.py
@@ -6,10 +6,11 @@ tree.create_node("/", "/")
 def add_path(path):
     split = path.split("/")
     with open("paths.txt", "a") as file:
-        file.write()
+        file.write(path)
     
     for i in range(len(split)):
         if i != 0:
             tree.create_node(split[i], split[i], parent=split[i-1])
         else:
             tree.create_node(split[i], split[i], parent="/")
+
