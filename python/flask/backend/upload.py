3d946e2824a1efd35caa71727d1e45ac7659c42a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 17:00:56 +0000

    stuff

--- python/flask/backend/upload.py
+++ python/flask/backend/upload.py
@@ -4,6 +4,7 @@ from flask import Blueprint, request
 upload = Blueprint(__name__, "upload")
 
 UPLOAD_FOLDER = Path(Path.cwd() / "files")
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
     return '.' in filename and \
