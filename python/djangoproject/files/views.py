f2097fb1fb6aa58a6c50d3407b9ac238f777f3a3
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 10:01:58 +0000

    stuff

--- python/djangoproject/files/views.py
+++ python/djangoproject/files/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 def file(request):
