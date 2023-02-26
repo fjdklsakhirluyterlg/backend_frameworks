aa04ea9edfdcfb5e347e1c45116dce7fd1561749
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 10:05:53 +0000

    stuff

--- python/djangoproject/files/forms.py
+++ python/djangoproject/files/forms.py
@@ -1,7 +1,5 @@
 from django import forms
 
 class DocumentForm(forms.Form):
-    docfile = forms.FileField(
-        label='Select a file',
-        help_text='max. 42 megabytes'
-    )
+    title = forms.CharField(max_length=50)
+    file = forms.FileField()
