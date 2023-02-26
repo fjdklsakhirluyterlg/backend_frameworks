516d22246a9e1e14f73661bb176f0feec05de20e
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 16:59:21 +0000

    stuff

--- python/flask/backend/upload.py
+++ python/flask/backend/upload.py
@@ -3,6 +3,8 @@ from flask import Blueprint, request
 
 upload = Blueprint(__name__, "upload")
 
+UPLOAD_FOLDER = Path(Path.cwd())
+
 @upload.route("/upload")
 def upload_file():
     if request.method == "POST":
