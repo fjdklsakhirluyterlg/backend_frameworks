364419490a75b96fc59691ac3410f14ccfe5c080
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 09:06:51 +0000

    stuff

--- python/djangoproject/files/models.py
+++ python/djangoproject/files/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+import os
+from uuid import uuid4
+
+def file_upload_path(instance, filename):
+    _, ext = os.path.splitext(filename)
+    new_filename = f'{uuid4().hex}{ext}'
+    return os.path.join('myfiles', str(instance.id), new_filename)
+
 # Create your models here.
 
 class UploadedFile(models.Model):
