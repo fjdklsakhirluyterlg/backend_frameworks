0022c50b247e68e5c42b4f174cd623b30a805b0a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 17:01:50 +0000

    stuff

--- python/flask/backend/upload.py
+++ python/flask/backend/upload.py
@@ -18,5 +18,22 @@ def upload_file():
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
+    
+    if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('download_file', name=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
         
         
