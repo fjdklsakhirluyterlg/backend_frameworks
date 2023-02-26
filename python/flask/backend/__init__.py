1fc9c0792a9e301cddb718f41792f607d4aef309
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 16:55:57 +0000

    stuff

--- python/flask/backend/__init__.py
+++ python/flask/backend/__init__.py
@@ -6,13 +6,14 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-
+    
     app.config['SQLALCHEMY_DATABASE_URI'] =f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
 
     from .routes import routes
 
     app.register_blueprint(routes)
 
-    db.init_app(app)
 
     return app
