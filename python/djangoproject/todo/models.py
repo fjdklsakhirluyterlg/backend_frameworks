81b1c76bd7b331b5e1a1834994694d30e47298df
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 16:39:44 +0000

    stuff

--- python/djangoproject/todo/models.py
+++ python/djangoproject/todo/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 
 class Todo(models.Model):
     name = models.CharField(max_length=350)
     done = models.BooleanField()
-    tiemstamp = models.DateTimeField()
+    tiemstamp = models.DateTimeField(default=now())
