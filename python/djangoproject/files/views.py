0f85135af132142b83508263670ed67882eb7c06
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 10:02:46 +0000

    stuff

--- python/djangoproject/files/views.py
+++ python/djangoproject/files/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
+
+from .forms import DocumentForm
 # Create your views here.
 
 def file(request):
     if request.method == "POST":
-        request.files
+        form = DocumentForm(request.POST, request.FILES)
