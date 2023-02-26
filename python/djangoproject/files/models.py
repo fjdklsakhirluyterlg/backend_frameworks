783c3150c3981ce79e5b0ae16cdd42bab0d84dc5
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-26 09:29:19 +0000

    stuff

--- python/djangoproject/files/models.py
+++ python/djangoproject/files/models.py
@@ -6,11 +6,11 @@ from uuid import uuid4
 def file_upload_path(instance, filename):
     _, ext = os.path.splitext(filename)
     new_filename = f'{uuid4().hex}{ext}'
+    instance.original_filename = filename   # save the original filename in the model
+    instance.filename = new_filename   # save the new filename in the model
     return os.path.join('myfiles', str(instance.id), new_filename)
 
-# Create your models here.
-
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to=file_upload_path)
-    uploaded_at = models.DateTimeField(auto_now_add=True)
+    myfile = models.FileField(upload_to=file_upload_path)
     filename = models.CharField(max_length=255, blank=True, null=True)
+    original_filename = models.CharField(max_length=255, blank=True, null=True)
