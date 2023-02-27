0d33909317861470623ead0a80303411b0556b5e
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-27 22:18:39 +0000

    stuff

--- python/bottle/app.py
+++ python/bottle/app.py
@@ -4,4 +4,8 @@ from bottle import route, run, template
 def index(name):
     return template('<b>Hello {{name}}</b>!', name=name)
 
+@route("/test")
+def test():
+    return template()
+
 run(host='localhost', port=8080)
