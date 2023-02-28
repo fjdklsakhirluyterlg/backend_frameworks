05a06c4293a210e1000f82e3d121e2627c6d0d39
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-28 22:01:28 +0000

    stuff

--- python/djangoproject/todo/models.py
+++ python/djangoproject/todo/models.py
@@ -6,4 +6,4 @@ from django.utils.timezone import now
 class Todo(models.Model):
     title = models.CharField(max_length=350)
     done = models.BooleanField(default=False)
-    tiemstamp = models.DateTimeField(default=now())
+    tiemstamp = models.DateTimeField(default=now)
