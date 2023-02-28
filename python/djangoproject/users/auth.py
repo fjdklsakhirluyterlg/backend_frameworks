7f6e97bd7928d6fe483ecf39820580b817cd9a27
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-28 22:08:51 +0000

    stuff

---
+++ python/djangoproject/users/auth.py
@@ -0,0 +1,11 @@
+rom django.shortcuts import render, redirect
+
+from django.contrib.auth import authenticate, login
+
+from django.contrib.auth.forms import UserCreationForm
+
+def signup(request):
+
+    if request.user.is_authenticated:
+
+        return redirect(‘/’)
