9e74a81b81501d8acff357eb6c5baa6a5f96eed8
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-28 22:10:08 +0000

    stuff

--- python/djangoproject/users/auth.py
+++ python/djangoproject/users/auth.py
@@ -7,5 +7,22 @@ from django.contrib.auth.forms import UserCreationForm
 def signup(request):
 
     if request.user.is_authenticated:
-
         return redirect("/")
+    
+    if request.method == ‘POST’:
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            username = form.cleaned_data.get(‘user’)
+
+            password = form.cleaned_data.get(‘pass’)
+
+            user = authenticate(username=user, password=pass)
+
+            login(request, user)
+
+            return redirect(‘/’)
