98d4d9c5be8d4fdc6977b72f77ed2c492844b1f7
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 22:49:14 +0000

    stuff

--- ruby/ruby_on_rails/config/routes.rb
+++ ruby/ruby_on_rails/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 end
 
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
+  get "/article", to: "articles#index"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
