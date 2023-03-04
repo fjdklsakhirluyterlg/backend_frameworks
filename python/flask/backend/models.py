7059981dcbf950684262b954dad2397f3b51b70a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 21:18:42 +0000

    stuff

--- python/flask/backend/models.py
+++ python/flask/backend/models.py
@@ -3,8 +3,10 @@ from flask_login import UserMixin
 
 
 class todo(db.Model):
+    id - db.Column(db.integer, primary_key=True)
     title = db.Column(db.String(350))
     completed = db.Column(db.Boolean, default=False)
 
-class User(db.Model):
+class User(db.Model, UserMixin):
+
     name = db.Column(db.Text)
