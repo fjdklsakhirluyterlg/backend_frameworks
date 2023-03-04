ec11a36641c39c2fce1851e195703b3a65a8a130
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 22:01:39 +0000

    stuff

---
+++ dashboard/main.go
@@ -0,0 +1,15 @@
+package dashboard
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+    r := gin.Default()
+
+    r.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "Hello, World!",
+        })
+    })
+
+    r.Run(":8080")
+}
