8e3c6985b50cd895c27a7d88a65f38d41d310007
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 21:06:45 +0000

    stuff

--- python/flask/backend/models.py
+++ python/flask/backend/models.py
@@ -3,3 +3,6 @@ from . import db
 class todo(db.Model):
     title = db.Column(db.String(350))
     completed = db.Column(db.Boolean, default=False)
+
+class User(db.Model):
+    name = db.Column(db.Text)
