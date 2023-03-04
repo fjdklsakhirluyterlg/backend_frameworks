099c778580f0062d5e2cec02fc13c1b3fcb64fed
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 21:43:01 +0000

    stuff

--- python/djangoproject/users/managers.py
+++ python/djangoproject/users/managers.py
@@ -3,11 +3,11 @@ from django.contrib.auth.base_user import BaseUserManager
 class UserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
+    def _create_user(self, username, email, password, **extra_fields):
         if not email:
             raise ValueError('The given email must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(user_name=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
