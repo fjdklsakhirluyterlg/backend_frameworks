2793eaab8b55230b1bca466cbd11df337a628292
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 21:29:27 +0000

    stuff

--- python/djangoproject/users/models.py
+++ python/djangoproject/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-
+from django.core.mail import send_mail
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
@@ -19,3 +19,4 @@ class User(AbstractBaseUser, PermissionsMixin):
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
 
     objects = UserManager()
+
