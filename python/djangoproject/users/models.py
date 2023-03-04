856e6322869ada9313ac72e735f621a8f80b0edd
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 21:28:37 +0000

    stuff

--- python/djangoproject/users/models.py
+++ python/djangoproject/users/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 
+from .managers import UserManager
+
 # Create your models here.
 
 
