b69dbeaa1f27b363807760f399d50e631c6b920d
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 22:23:40 +0000

    stuff

--- dashboard/tester/testing.go
+++ dashboard/tester/testing.go
@@ -1,6 +1,11 @@
 package main
 
-func main(){
-	servers = [...]string{"http://127.0.0.1:5080"}
+import "fmt"
 
+func main(){
+	servers := [...]string{"http://127.0.0.1:5080"}
+
+	for i, val := range servers{
+		fmt.Println(val)
+	}
 }
