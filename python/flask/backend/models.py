0babacaa4d88a8f2d29a7d92c04cf41c711e8e9a
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-04 21:20:27 +0000

    stuff

--- python/flask/backend/models.py
+++ python/flask/backend/models.py
@@ -10,3 +10,5 @@ class todo(db.Model):
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(50))
