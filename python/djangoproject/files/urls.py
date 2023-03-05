ee0d00be61b899e2d3ecffae8e329b03b3532238
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-05 22:34:04 +0000

    stuff

--- python/djangoproject/files/urls.py
+++ python/djangoproject/files/urls.py
@@ -6,14 +6,14 @@ BASE_DIR = Path.cwd() if "files" not in Path.cwd() else Path.parent()
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-from django.conf import settings
-from django.conf.urls.static import static
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('file', views.file, name="file"),
     path('success', views.success, name="success"),
 ]
+
+from django.conf import settings
+from django.conf.urls.static import static
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
