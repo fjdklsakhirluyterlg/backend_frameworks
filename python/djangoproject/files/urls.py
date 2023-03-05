150fc94c240f7b31d070f016148e879d411ef77f
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-05 22:35:42 +0000

    stuff

--- python/djangoproject/files/urls.py
+++ python/djangoproject/files/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 from pathlib import Path
 
-BASE_DIR = Path.cwd() if "files" not in Path.cwd() else Path.parent()
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('file', views.file, name="file"),
