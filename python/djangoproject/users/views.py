19d0e2c88dd89c10c19e6e1089682a3b24aa71de
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-07 22:57:36 +0000

    stuff

--- python/djangoproject/users/views.py
+++ python/djangoproject/users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def handle_avataer_upload(file):
-    file.filename
+    filename = file.filename
 
 def upload_avatar(request):
     if request.method == "POST":
