5810bcbc768a1121f5c5ebf68c9ca3f288620e0e
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-03-13 22:35:32 +0000

    stuff

--- python/flask/backend/auth.py
+++ python/flask/backend/auth.py
@@ -4,7 +4,7 @@ from flask_login import login_user
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/auth/login")
+@auth.route("/auth/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         data = request.get_json()
